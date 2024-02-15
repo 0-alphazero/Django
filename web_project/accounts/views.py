e52a140cbecc5fa70bebae619ee19cf6522fd5db
0-alphazero <abdullah.zahid247@hotmail.com>
2024-02-15 23:45:18 +0500

    15-2-24

--- web_project/accounts/views.py
+++ web_project/accounts/views.py
@@ -1,8 +1,8 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import *
-
+from .forms import OrderForm
 
 def home(request):
     orders =  Order.objects.all()
@@ -34,8 +34,36 @@ def customer(request, pk):
     return render(request, 'accounts/customer.html',context)
 
 def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        #print("printing POST:", request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
-    context = {}
+    context = {'form' :form}
     return render(request, 'accounts/order_form.html', context)
 
-# Create your views here.
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    form = OrderForm(instance=order)
+    context ={'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context= {'item' :order}
+    return render(request, 'accounts/delete.html', context)
